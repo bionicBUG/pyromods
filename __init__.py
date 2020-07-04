bf727cfa13224246e84a2f5d548b991d6af771a8
Ojas Sinha <sinhaojas67@gmail.com>
2020-07-04 14:07:35 +0000

    new markdown module!

--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
 from .ip import Ip
 from .swreport import SpamWatchReport
 from .download import Download
+from .markdowns import Markdowns
---
+++ markdowns.py
@@ -0,0 +1,37 @@
+from pyrobud import command, module
+
+class Markdowns(module.Module):
+    name: str="Markdowns"
+    disabled: bool= False
+
+    @command.desc("Bold")
+    @command.alias("b")
+    async def cmd_bold(self, ctx: command.Context) -> str:
+        if not ctx.input:
+            return "**Enter the text**"
+        text = ctx.input
+        return "**" + text + "**"
+
+    @command.desc("Italics")
+    @command.alias("it")
+    async def cmd_italics(self, ctx: command.Context) -> str:
+        if not ctx.input:
+            return "__Enter the text__"
+        text = ctx.input
+        return "__" + text + "__"
+
+    @command.desc("Strikethrough")
+    @command.alias("sth")
+    async def cmd_under(self, ctx: command.Context) -> str:
+        if not ctx.input:
+            return "~~Enter the text~~"
+        text = ctx.input
+        return "~~" + text + "~~"
+
+    @command.desc("Monospace")
+    @command.alias("mo")
+    async def cmd_mono(self, ctx: command.Context) -> str:
+        if not ctx.input:
+            return "`Enter the text`"
+        text = ctx.input
+        return "'" + text + "'"
